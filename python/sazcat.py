65469308b268c0bd9894f7d9ac815702554a0b9c
VoltBit <andrei_ciprian.dobre@cti.pub.ro>
2017-10-16 23:49:11 +0300

    file name format

--- python/sazcat.py
+++ python/sazcat.py
@@ -29,20 +29,35 @@ class Sazcat:
         self.__repack()
 
     def __unpack(self):
+        self.capture_files = 0
         for file in self.files:
             with ZipFile(file) as saz_file:
                 file_name = file.split('/')[-1]
                 self.dir_map[self.ID] = file_name
                 self.ID += 1
                 saz_file.extractall(self.DST + file_name)
+                self.capture_files += (len([x for x in saz_file.namelist() if "raw/" in x]) - 1)
+        self.capture_files /= 3
+        print("Total files: ", self.capture_files)
 
     def __merge(self):
-        total_files = 0
+        offset = 0
+        name_format = "{:1}"
+        if self.capture_files > 9:
+            name_format = "{:02}"
+        elif self.capture_files > 99:
+            name_format = "{:03}"
+        elif self.capture_files > 999:
+            name_format = "{:04}"
+
         for dir_id in range(0, self.ID):
-            print(os.listdir(self.DST + self.dir_map[dir_id] + '/raw/'))
-
-
-        #     total_files += len(os.listdir(self.dir_map[dir_id]))
+            fl = sorted(os.listdir(self.DST + self.dir_map[dir_id] + '/raw/'))
+            sessions = [fl[i:i+3] for i in range(0, len(fl), 3)]
+            # print(sessions)
+            for session in sessions:
+                print(name_format.format(int(session[0].split('_')[0])))
+            file_count = len(os.listdir(self.DST + self.dir_map[dir_id] + '/raw/'))
+            offset += file_count
 
     def __repack(self):
         pass
